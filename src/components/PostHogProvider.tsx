816f57e8399b9ebbe3a55558726ee03157faa67e
Manu <manujasan23@gmail.com>
2026-03-31 20:40:49 +0530

    posthog fixes

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -8,6 +8,7 @@ export default function PostHogProvider({ children }: { children: React.ReactNod
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: '2026-01-30',
       person_profiles: 'identified_only',
     })
   }, [])
